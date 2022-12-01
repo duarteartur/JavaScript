75b852e656f1d7abf75c4257e66a9ef5ee6eccb1
Artur Amorim <62303570+duarteartur@users.noreply.github.com>
2022-12-01 11:12:17 -0300

    Update DateManipulation.js

--- DateManipulation.js
+++ DateManipulation.js
@@ -3,3 +3,25 @@ function dateToYYYYMM(fcData) {
     fcData = fcData.replace(/-/g, '');
     return fcData.substring(0, (fcData.length - 2));
 } 
+
+function SomarMeses(data, meses) {
+    data = data.split('/');
+
+    var dia = parseInt(data[0], 10);
+    var mesfuturo = parseInt(data[1], 10) + meses;
+    var ano = parseInt(data[2], 10);
+
+    if (mesfuturo > 12) {
+        while (mesfuturo > 12) {
+            mesfuturo -= 12;
+            ano++;
+        }
+    }
+
+    if (mesfuturo < 10) {
+        mesfuturo = '0' + mesfuturo;
+    }
+
+    return dia + "/" + mesfuturo + "/" + ano;
+
+}
